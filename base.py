f2544ba3780444562b3c6cb6dfee0fc99e8292d5
mauro <mauro@aeon>
2012-07-08 11:20:36 -0300

    base encoding/decoding.

---
+++ base.py
@@ -0,0 +1,20 @@
+import string
+BASE_LIST = string.digits + string.letters + '_@'
+BASE_DICT = dict((c, i) for i, c in enumerate(BASE_LIST))
+
+def base_decode(string, reverse_base=BASE_DICT):
+    length = len(reverse_base)
+    ret = 0
+    for i, c in enumerate(string[::-1]):
+        ret += (length ** i) * reverse_base[c]
+
+    return ret
+
+def base_encode(integer, base=BASE_LIST):
+    length = len(base)
+    ret = ''
+    while integer != 0:
+        ret = base[integer % length] + ret
+        integer /= length
+
+    return ret
