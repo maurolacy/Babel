6dcf838e7cf9e58888eea9eb2d22eff7c4b073e8
mauro <mauro@aeon>
2012-07-08 10:50:26 -0300

    better version.

--- pi4.py
+++ pi4.py
@@ -1,4 +1,4 @@
 from bigfloat import const_pi, precision
 from string import translate, maketrans
-print translate(str(const_pi(precision(5482))), maketrans('0123456789', '.Hdlro lwe'))
-print translate(str(const_pi(precision(24746))), maketrans('0123456789', 'rd.eHwlo! '))
+#print translate(str(const_pi(precision(5482))), maketrans('0123456789', '\nHdlro lwe'))
+print translate(str(const_pi(precision(24746))), maketrans('0123456789', 'rd\neHwlo! '))
