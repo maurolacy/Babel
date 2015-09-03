bb74517e42298083a4e9e7b5e7c093257bc32e15
Mauro Lacy <mauro@lacy.com.ar>
2015-09-03 16:24:21 -0300

    BabelSearch script, and translation table builder helper

---
+++ BabelSearch.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+
+PHRASE="$@"
+[ -z "$PHRASE" ] && echo "Usage: $0 <phrase>" && exit 1
+
+# Default number of digits to search for
+DIGITS=1000000
+
+# Min numerical base to search
+MIN=`echo -n "$PHRASE" | sed 's/\(.\)/\1\n/g' | sort -u | wc -l`
+# Max numerical base to search
+MAX=62
+
+echo $PHRASE
+REGEX=`./Babel.pl "$PHRASE"`
+
+for B in `seq $MIN $MAX`
+do
+    echo $B
+    KEY=`./pi.sh $B $REGEX $DIGITS`
+    LEN=`echo -n $KEY | wc -c`
+    if [ $LEN -lt $DIGITS ]
+    then
+        break
+    fi
+done
+
+if [ $LEN -lt $DIGITS ]
+then
+    REGEX2=`echo $REGEX | sed 's/\.\*?//'`
+    TR="`./tr.py $KEY \"$PHRASE\"`"
+    D=`./pi.m $DIGITS $B | perl -pe "$REGEX2" | wc -c`
+    D=`expr $D - 2`
+    ./pi.m $D $B | tr 0-9A-Za-z "$TR"
+    echo "Perlre: $REGEX"
+    echo "Base  : $B"
+    echo "Digits: $D"
+    echo "Key   : $KEY"
+    echo -n "Table : " #0-9A-Za-z $TR"
+    if [ $B -gt 10 ]
+    then
+        echo -n 0-9
+        if [ $B -gt 36 ]
+        then
+            echo -n A-Z
+            D=`expr 60 + $B`
+            D=`gbase -d $D | grep ^Hex: | cut -f2 -d\ `
+            echo -n -e "a-\x$D"
+        else
+            D=`expr 54 + $B`
+            D=`gbase -d $D | grep ^Hex: | cut -f2 -d\ `
+            echo -n -e "A-\x$D"
+        fi
+    else
+        D=`expr 47 + $B`
+        D=`gbase -d $D | grep ^Hex: | cut -f2 -d\ `
+        echo -n -e "0-\x$D"
+    fi
+    echo -n " "
+    BACKS=`echo -n "$TR" | sed 's/[^\\]*//g' | wc -c`
+    echo "$TR" | sed "s/\(.\{$[B + $BACKS]\}\).*/\1/"
+fi
---
+++ tr.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+""" Generates translation table for a given key string """
+
+from __future__ import print_function
+from string import digits, letters
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        print("Usage: %s <key> <phrase>" % sys.argv[0])
+        sys.exit(1)
+    k = sys.argv[1]
+    p = sys.argv[2]
+    if len(k) != len(p):
+        print("Key and phrase must be the same length.")
+        sys.exit(1)
+    d={}
+    for key in digits + letters:
+        d[key] = key # default table
+
+    for key, val in zip(k, p):
+        d[key]=val
+
+    for key in sorted(d):
+        if d[key] in (' ', '!', '?'): # escape these
+            print('\%s' % d[key], end='')
+        else:
+            print(d[key], end='')
+    print()
