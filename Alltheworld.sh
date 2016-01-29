9739458db354c048dd25607dcc1cbe5b07616bc7
Mauro Lacy <mauro@lacy.com.ar>
2016-01-29 09:20:12 -0300

    "All the world's a stage," ex.

--- Alltheworld.sh
+++ Alltheworld.sh
@@ -1,2 +1,3 @@
 :
-./pi.m 27038 27 | tr 0-9A-Q redo4sashte\'tD\ w\ H,JgAMl\ ll
+#./pi.m 27038 27 | tr 0-9A-Q redo4sashte\'tD\ w\ H,JgAMl\ ll
+./pi 27038 27 | tr 0-9A-Q redo4sashte\'tD\ w\ H,JgAMl\ ll
