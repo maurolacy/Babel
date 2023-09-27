36997d9c88c74d66cb4fe390397b1a5ff9313383
Mauro Lacy <mauro@lacy.com.es>
2023-09-27 17:35:26 +0200

    Remove broken deprecated bigfloat utils

--- pi2.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python3 
-
-import sys
-from bigfloat import *
-#from string import *
-#print(translate(str(const_pi(precision(209276))), maketrans('0123456789', 'del l!owrH')))
-print(const_pi(precision(int(sys.argv[1]))))
--- pi4.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-from bigfloat import const_pi, precision
-from string import digits, maketrans, translate
-
-
-print(translate(str(const_pi(precision(24746))), maketrans(digits, 'rd\neHwlo! ')))
-#print()
-#print(translate(str(const_pi(precision(5482))), maketrans(digits, '.Hdlro lwe')))
